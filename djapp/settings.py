93afeda59929372bcefc0422f110014542a2f27e
Dmitriy <parrot-studio@hotmail.com>
2018-09-18 19:38:56 +0300

    added allowed hosts

--- djapp/settings.py
+++ djapp/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'wamcg(0c21#(r-5_)2sai9&9%u!0ebktyoe=57(5hm)=u2ygnf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '89.223.95.235'
+]
 
 
 # Application definition
